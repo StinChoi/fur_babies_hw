c3667f7e3323741a492b1e8c6bd7324a92420203
Austin <austinchoi22@gmail.com>
2021-12-02 15:00:31 -0700

    Initial set up for About

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from "./components/NavBar";
 import { Route, Routes } from "react-router";
 import Pets from "./components/Pets"
+import About from "./components/About";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/pets" element={<Pets />} />
+        <Route path="/about" element={<About />} />
       </Routes>
 
     </div>
